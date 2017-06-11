0a6156401e54a4d87ff8d178b415983a2d9351bf
Luka Popijac <luka.popijac@gmail.com>
2017-06-10 22:01:30 -0700

    second initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# cholesky-tools
+Cholesky Tools
+==============
+
 Basic operations for symmetric positive-definite matrices.
+
+
+License
+-------
+This software is released under the MIT license.
---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+	determinant: determinant,
+	inverse: inverse,
+	cholesky: cholesky
+};
+
+function determinant(X) {
+	// TODO
+}
+
+function inverse(X) {
+	// TODO
+}
+
+function cholesky(X) {
+	// TODO
+}
